28d1990be1f8880cd1bd77e0d8dd8aece0ed03a1
marcogracie <marco.gracie22@gmail.com>
2025-05-08 16:36:24 -0400

    feat(4): CSS related things.

--- personal-site/src/components/layout/index.tsx
+++ personal-site/src/components/layout/index.tsx
@@ -1,19 +1,17 @@
-import './index.css'
-import Header from '../header';
-import { Outlet } from 'react-router-dom';
+import "./index.css";
+import Header from "../header";
+import { Outlet } from "react-router-dom";
 
 /**
  * Main layout of website
  */
 const Layout = () => (
-    <>
-        <Header/>
-        <div id='main' className='main'>
-            <div id='right_main' className='right_main'>
-                <Outlet />
-            </div>
-        </div>
-        </>
+  <>
+    <Header />
+    <div id="main" className="main">
+      <Outlet />
+    </div>
+  </>
 );
 
 export default Layout;
--- personal-site/src/components/main/landingPage/index.tsx
+++ personal-site/src/components/main/landingPage/index.tsx
@@ -1,19 +1,27 @@
+import "./index.css";
 /**
  * LandingPage Component renders the main page of the website
  */
 const LandingPage = () => {
   return (
-    <>
+    <div className="landingPage">
       <div className="imageContainer">
-        <img src="https://google.com" alt="Marco Gracie" />
+        <img src="https://picsum.photos/200/300" alt="Marco Gracie" />
       </div>
       <div className="paragraphContainer">
         <p>
           Hi, I'm Marco Gracie. A software developer interested in the
-          intersection of Human Interaction and computing, along with AI.
+          intersection of Human Interaction and computing, along with AI. Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+          ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia
+          deserunt mollit anim id est laborum.
         </p>
       </div>
-    </>
+    </div>
   );
 };
 
