e3a93c48bcb54a9831ecda97b0eec57343f606e5
marcogracie <marco.gracie22@gmail.com>
2025-05-09 14:42:23 -0400

    feat(5): Header Routes setup, css may need ironing out.

--- personal-site/src/components/header/index.tsx
+++ personal-site/src/components/header/index.tsx
@@ -1,4 +1,5 @@
 import "./index.css";
+import { NavLink } from "react-router-dom";
 
 /**
  * Header Component that renders submenus and title
@@ -8,6 +9,35 @@ const Header = () => {
     <div id="header" className="header">
       <div className="left-area">
         <h1> Marco's Website</h1>
+      </div>
+      <div className="center-area">
+        <NavLink
+          to="/"
+          id="landing-page"
+          className={({ isActive }) =>
+            `menu_button ${isActive ? "menu_selected" : ""}`
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/projects"
+          id="projects-page"
+          className={({ isActive }) =>
+            `menu_button ${isActive ? "menu_selected" : ""}`
+          }
+        >
+          Projects
+        </NavLink>
+        <NavLink
+          to="/contact"
+          id="contact-page"
+          className={({ isActive }) =>
+            `menu_button ${isActive ? "menu_selected" : ""}`
+          }
+        >
+          Contact
+        </NavLink>
       </div>
     </div>
   );
