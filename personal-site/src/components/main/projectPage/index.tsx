a99a0301483d8a12100ccc45c67052303d924e19
marcogracie <marco.gracie22@gmail.com>
2025-05-09 15:53:10 -0400

    feat(6): Scrolling effect for projects is done, have to make a good way for indicating there are more projects

---
+++ personal-site/src/components/main/projectPage/index.tsx
@@ -0,0 +1,16 @@
+import "./index.css";
+
+/**
+ * ProjectPage Component renders the project page of the website
+ */
+const ProjectPage = () => {
+  return (
+    <div className="projects">
+      <div className="project"> Project 1 </div>
+      <div className="project"> Project 2 </div>
+      <div className="project"> Project 3 </div>
+    </div>
+  );
+};
+
+export default ProjectPage;
